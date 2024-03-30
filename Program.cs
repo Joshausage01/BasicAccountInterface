13ec2a787c288224ca569317f28e89f984940e97
Joshausage01 <159090739+Joshausage01@users.noreply.github.com>
2024-03-30 09:40:49 +0800

    Add files via upload
    Signed-off-by: Joshausage01 <159090739+Joshausage01@users.noreply.github.com>

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+    NAME: CRISOSTOMO, JOSHUA B.
+    COURSE/SECTION: BSCPE 1-1
+*/
+using System;
+public class BasicAccountInterface
+{
+    static void Main(String[] args)
+    {
+        string realUsername = "Admin", realPassword = "Admin1234!";
+        int totalLogIns = 4;
+        
+        while (totalLogIns > 0)
+        {
+            Console.Write("\nEnter Username: ");
+            string username = Console.ReadLine();
+            Console.Write("Enter Password: ");
+            string password = Console.ReadLine();
+
+            if (username == realUsername && password == realPassword)
+            {
+                Console.WriteLine("\nAccount log-in successful.");
+                break;
+            }
+            else
+            {
+                totalLogIns--;
+                if (totalLogIns > 0)
+                {
+                    Console.WriteLine("Incorrect credentials. Please try again!");
+                    Console.WriteLine("Number of Log-in attempt/s left: " + totalLogIns);
+                }
+                else
+                {
+                    Console.WriteLine("The username or the password is incorrect.");
+                    Console.WriteLine("\nToo many unsuccessful Log-in attempts. Account Locked.");
+                    return;
+                }
+            }
+        }
+    }
+}
